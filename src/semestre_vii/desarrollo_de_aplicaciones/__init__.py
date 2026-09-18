7764d8eca02edddd93c9a7e0d5c9d35caca3e05d
Fernando Leon Franco <fernanlee2131@gmail.com>
2026-09-18 02:15:29 -0600

    feat(apps): registrar rutas de la materia

---
+++ src/semestre_vii/desarrollo_de_aplicaciones/__init__.py
@@ -0,0 +1,7 @@
+"""Proyectos de la materia Desarrollo de Aplicaciones."""
+
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parents[3]
+DATA_DIR = ROOT_DIR / "data" / "desarrollo_de_aplicaciones"
+MODELS_DIR = ROOT_DIR / "models" / "desarrollo_de_aplicaciones"
