cae167a2bbc06866e299d6dd948139a1b661181f
Fernando Leon Franco <fernanlee2131@gmail.com>
2026-09-18 02:10:47 -0600

    feat(proyecto-2): crear rutas base del proyecto

---
+++ src/semestre_vii/aprendizaje_automatico_iii/proyecto_2/__init__.py
@@ -0,0 +1,7 @@
+"""Pronóstico de demanda de combustible para la red de gasolineras."""
+
+from semestre_vii.aprendizaje_automatico_iii import DATA_DIR as COURSE_DATA_DIR
+from semestre_vii.aprendizaje_automatico_iii import MODELS_DIR as COURSE_MODELS_DIR
+
+PROJECT_DATA_DIR = COURSE_DATA_DIR / "proyecto_2"
+PROJECT_MODELS_DIR = COURSE_MODELS_DIR / "proyecto_2"
